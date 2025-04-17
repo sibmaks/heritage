4bfe328e4fd33fe6ec43c0360e4e75bc6032a7f6
sibmaks <sibmaks@vk.com>
2025-04-17 13:50:01 +0300

    Initial commit

---
+++ src/main/kotlin/ru/nikita/heritage/HeritageApplication.kt
@@ -0,0 +1,11 @@
+package ru.nikita.heritage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HeritageApplication
+
+fun main(args: Array<String>) {
+	runApplication<HeritageApplication>(*args)
+}
---
+++ src/test/kotlin/ru/nikita/heritage/HeritageApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.nikita.heritage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HeritageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
