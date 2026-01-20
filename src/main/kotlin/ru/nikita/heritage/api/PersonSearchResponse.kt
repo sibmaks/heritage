a050991d04017a1cfa034d051f4851ae4d38dcf7
Drobyshev Maksim <sibmaks@vk.com>
2026-01-20 17:24:05 +0300

    Add Swagger descriptions for person search

--- src/main/kotlin/ru/nikita/heritage/api/PersonSearchResponse.kt
+++ src/main/kotlin/ru/nikita/heritage/api/PersonSearchResponse.kt
@@ -1,11 +1,16 @@
 package ru.nikita.heritage.api
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 /**
  *
  * @author sibmaks
  * @since 0.0.1
  */
+@Schema(description = "Response payload for person search.")
 data class PersonSearchResponse(
+    @field:Schema(description = "List of matching persons.")
     val persons: List<PersonSearchItem>,
+    @field:Schema(description = "True when there are more matches than the configured limit.")
     val hasMore: Boolean,
 )
